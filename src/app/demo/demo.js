a4cd376d4c53f92f67919ef421496afd36f71e1d
Bernard Lambeau <blambeau@gmail.com>
2014-07-10 17:13:49 +0200

    Add additional examples.

--- src/app/demo/demo.js
+++ src/app/demo/demo.js
@@ -25,17 +25,69 @@ angular.module( 'finitio.demo', [
   $scope.examples = [
     {
       id: "hello",
-      order: 0,
       label: "Hello World!",
       schema: ".",
       data: '"Hello World"'
     },
     {
-      id: "basic",
-      order: 1,
-      label: "Basic",
+      id: "separator"
+    },
+    {
+      id: "integer",
+      label: "Integer",
       schema: "@import finitio/data\n\nInteger",
       data: "12"
+    },
+    {
+      id: "sbyc",
+      label: "Specialization by Constraint (SByC)",
+      schema: "@import finitio/data\n\nInteger( i | i>0 )\n",
+      data: "-16"
+    },
+    {
+      id: "union",
+      label: "Union types",
+      schema: "@import finitio/data\n\nInteger | Nil\n",
+      data: "16"
+    },
+    {
+      id: "sequence",
+      label: "Sequences",
+      schema: "@import finitio/data\n\n[ Integer( i | i>0 ) ]\n",
+      data: "[2015, 16, 12]"
+    },
+    {
+      id: "set",
+      label: "Sets",
+      schema: "@import finitio/data\n\n{ Integer( i | i>0 ) }\n",
+      data: "[2015, 16, 12, 16]"
+    },
+    {
+      id: "tuple",
+      label: "Tuples",
+      schema: "@import finitio/data\n\nPassword = String( s | s.length >= 8 )\n\n{\n  name: String\n  password: Password\n}\n",
+      data: '{\n  "name": "Bernard Lambeau",\n  "passwor": "finitio"\n}'
+    },
+    {
+      id: "form",
+      label: "Tuples + SByC",
+      schema: "@import finitio/data\n\nPassword = String( s | s.length >= 8 )\n\n{\n  name: String\n  password: Password\n  confirm: Password\n}( t | samepass: t.password == t.confirm )\n",
+      data: '{\n  "name": "Bernard Lambeau",\n  "password": "finitio123",\n  "confirm": "finitio789"\n}'
+    },
+    {
+      id: "relation",
+      label: "Relations",
+      schema: "@import finitio/data\n\nTalk = {\n  name: String\n  by: String\n}\n\n{Talk}",
+      data: '[\n  {\n    "name": "Data Deserves a Language Too",\n    "by": "Bernard Lambeau"\n  },\n  {\n    "name": "CSV on the Web",\n    "by": "Jeni Tennison"\n  }\n]'
+    },
+    {
+      id: "separator"
+    },
+    {
+      id: "date",
+      label: "Dates ?",
+      schema: "@import finitio/data\n\nDate\n",
+      data: '"2015-07-15"'
     }
   ];
 
@@ -61,7 +113,7 @@ angular.module( 'finitio.demo', [
       try {
         coerced = system.dress(JSON.parse(data));
         $scope.status = "success";
-        jstype = coerced.constructor;
+        jstype = coerced && coerced.constructor;
         if (jstype) {
           jstype = jstype.name;
         } else {
